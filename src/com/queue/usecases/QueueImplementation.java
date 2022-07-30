e2fa24ea7d47b6726a8db8baee0d9ecf53f1f7bc
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-07-30 12:36:33 +0530

    Added : Node.java and QueueImplementation.java with add() & print()methods in file

---
+++ src/com/queue/usecases/QueueImplementation.java
@@ -0,0 +1,51 @@
+package com.queue.usecases;
+
+import java.util.Queue;
+
+public class QueueImplementation<T> {
+	Node<T> front;
+	Node<T> rear;
+	public int size;
+	
+	public QueueImplementation() {
+		this.size=0;
+	}
+	// enqueue the element -uc1
+	public void add(T data) {
+		Node<T> newNode = new Node<>(data);
+		size++;
+		if (front == null && rear == null) {
+			front = rear = newNode;
+			return;
+		}
+		rear.next = newNode;
+		rear = rear.next;
+	}
+
+	public void printQueue() {
+		if (front == null && rear == null) {
+			System.out.println("Queue is empty");
+			return;
+		}
+		Node<T> currentNode = front;
+		while (currentNode != null) {
+			System.out.print(currentNode + " <- ");
+			currentNode = currentNode.next;
+		}
+		System.out.println(" ");
+	}
+
+	public int getSize() {
+		return size;
+	}
+	public static void main(String[] args) {
+		QueueImplementation<Integer> queueImplementation = new QueueImplementation<Integer>();
+		queueImplementation.add(56);
+		queueImplementation.add(30);
+		queueImplementation.add(70);
+
+		queueImplementation.printQueue();// 56 <- 30 <- 70 <-
+		System.out.println("Size of Queue :"+queueImplementation.getSize());
+
+	}
+}
